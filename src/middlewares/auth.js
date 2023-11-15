ea6a00cfd43809753d585a93e73c42edf84675e3
Belen Banegas <belenbanegasbanegas@gmail.com>
2023-11-15 15:39:05 -0300

    cookies-persistencia-token-login-logout-me

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,19 +1,20 @@
-import jwt from 'jsonwebtoken'
+// auth.js
 
-function auth(req, res, next) {
+import jwt from "jsonwebtoken";
 
+const auth = (req, res, next) => {
   const token = req.cookies.token;
   if (!token) {
-      return res.status(401).json({ message: 'No token, authorization denied' });
+    return res.status(401).json({ message: "No token, authorization denied" });
   }
 
   try {
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      req.userId = decoded.userId;
-      next();
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.userId = decoded.userId;
+    next();
   } catch (error) {
-      res.status(400).json({ message: 'Token is not valid' });
+    res.status(400).json({ message: "Token is not valid" });
   }
-}
+};
 
-export default auth
+export default auth;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,12 @@
 import express from "express";
 import usersRouter from "./users.js";
 const router = express.Router();
+import { getSecret, getMe } from "../controllers/users.js";
+import auth  from '../middlewares/auth.js'
 
 router.use("/users", usersRouter);
+
+router.get("/me", auth, getMe)
+router.get("/secret", getSecret)
 
 export default router;
