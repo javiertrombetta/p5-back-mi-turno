5ad8beb6faed81d7b45c7dea0e7b067febc53f39
Valentinguardia <valentin.guardia@outlook.com>
2023-11-15 18:16:03 -0300

    feature get branches

--- src/routes/branches.js
+++ src/routes/branches.js
@@ -19,4 +19,26 @@ router.post("/create", async (req, res) => {
   }
 });
 
+router.get("/allBranches", async (req, res) => {
+  try {
+    const allBranches = await Branch.findAll();
+    res.send(allBranches);
+  } catch (error) {
+    res.status(500).send("Error al obtener sucursales");
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const branch = await Branch.findByPk(req.params.id);
+    if (branch) {
+      res.send(branch);
+    } else {
+      res.status(404).send("Sucursal no encontrada");
+    }
+  } catch (error) {
+    res.status(500).send("Error al obtener sucursal");
+  }
+});
+
 export default router;
