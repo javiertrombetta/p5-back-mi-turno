181397a31e291c7e0555b8db79e0be35cf24c411
javiertrombetta <jitrombetta@hotmail.com>
2023-11-30 03:08:48 -0300

    bug: change text output for user mail

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -211,7 +211,7 @@ const userController = {
     try {
       const user = await User.findOne({ where: { email } });
       if (!user) {
-        return res.status(404).json({ message: "Usuario no encontrado." });
+        return res.status(404).json({ message: "Usuario no registrado." });
       }
       const resetToken = generateToken({ userId: user.dni });
       user.resetPasswordToken = resetToken;
