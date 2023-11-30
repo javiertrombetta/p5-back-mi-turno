0c3ad88d58f381b0380e4a6b97ea8a816f47d22e
FrancoPrandi <francoprandi55@gmail.com>
2023-11-30 16:21:58 -0300

    bug: menor changes

--- src/middlewares/rolesMiddleware.js
+++ src/middlewares/rolesMiddleware.js
@@ -1,6 +1,8 @@
 const checkRole = (roles) => {
+  
   return (req, res, next) => {
-    if (req.user && roles.includes(req.user.rol)) {
+    console.log(req.user)
+    if (req.user && roles.includes(req.user.role)) {
       next();
     } else {
       res.status(403).json({ message: "No estás autorizado a realizar esta acción." });
