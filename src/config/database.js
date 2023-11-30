44bd6d0f4bcf4113e7555a92b5872a0ea196bd60
javiertrombetta <jitrombetta@hotmail.com>
2023-11-30 19:08:53 -0300

    feat: seed with faker

--- src/config/database.js
+++ src/config/database.js
@@ -1,3 +1,4 @@
+/*
 import Sequelize from "sequelize";
 import { config } from "dotenv";
 
@@ -11,6 +12,21 @@ const dbName = process.env.NODE_ENV === 'production' ? process.env.PROD_DB_NAME 
 const dbUser = process.env.NODE_ENV === 'production' ? process.env.PROD_DB_USER : process.env.DEV_DB_USER;
 const dbPassword = process.env.NODE_ENV === 'production' ? process.env.PROD_DB_PASSWORD : process.env.DEV_DB_PASSWORD;
 const dbHost = process.env.NODE_ENV === 'production' ? process.env.PROD_DB_HOST : process.env.DEV_DB_HOST;
+
+const sequelize = new Sequelize(dbName, dbUser, dbPassword, {
+  host: dbHost,
+  dialect: "postgres",
+  logging: false,
+});
+
+export default sequelize;
+*/
+import Sequelize from "sequelize";
+
+const dbName = 'mtw'; // Reemplaza con el nombre de tu base de datos
+const dbUser = ''; // Tu nombre de usuario de PostgreSQL
+const dbPassword = ''; // Tu contraseña de PostgreSQL
+const dbHost = 'localhost'; // Host de tu servidor PostgreSQL
 
 const sequelize = new Sequelize(dbName, dbUser, dbPassword, {
   host: dbHost,
