72ec2300fd3fe967d83d8148978dfce72918eb2b
dadario23 <dandrada23@gmail.com>
2023-12-01 01:51:20 -0300

    integer a bigint in Branch, Business, Reservation, User

--- src/models/Reservation.js
+++ src/models/Reservation.js
@@ -3,53 +3,61 @@ import sequelize from "../config/database.js";
 
 class Reservation extends Sequelize.Model {}
 
-Reservation.init({
-  id: {
-    type: Sequelize.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
+Reservation.init(
+  {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    date: {
+      type: Sequelize.DATE,
+      defaultValue: Sequelize.NOW,
+      allowNull: false,
+    },
+    time: {
+      type: Sequelize.TIME,
+      allowNull: false,
+    },
+    state: {
+      type: Sequelize.ENUM(
+        "pendiente",
+        "confirmado",
+        "cancelado",
+        "finalizado",
+        "ausente"
+      ),
+      defaultValue: "pendiente",
+    },
+    clientName: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    clientPhone: {
+      type: Sequelize.BIGINT,
+    },
+    clientEmail: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    branchId: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      references: {
+        model: "branches",
+        key: "id",
+      },
+    },
+    userId: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      references: {
+        model: "users",
+        key: "dni",
+      },
+    },
   },
-  date: {
-    type: Sequelize.DATE,
-    defaultValue: Sequelize.NOW,
-    allowNull: false,
-  },
-  time: {
-    type: Sequelize.TIME,
-    allowNull: false,
-  },
-  state: {
-    type: Sequelize.ENUM("pendiente", "confirmado", "cancelado", "finalizado", "ausente"),
-    defaultValue: "pendiente",
-  },
-  clientName: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  clientPhone: {
-    type: Sequelize.INTEGER,
-  },
-  clientEmail: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  branchId: {
-    type: Sequelize.INTEGER,
-    allowNull: false,
-    references: {
-      model: 'branches',
-      key: 'id',
-    }
-  },
-  userId: {
-    type: Sequelize.INTEGER,
-    allowNull: false,
-    references: {
-      model: 'users',
-      key: 'dni',
-    }
-  }
-}, 
-{ sequelize: sequelize, modelName: "reservations" });
+  { sequelize: sequelize, modelName: "reservations" }
+);
 
 export default Reservation;
