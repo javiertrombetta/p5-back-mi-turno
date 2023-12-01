d014e3adb96b11463f0ca26ea7c637ef3203ea36
javiertrombetta <jitrombetta@hotmail.com>
2023-12-01 01:09:29 -0300

    bug: fix realtions errors

--- src/tests/seed.js
+++ src/tests/seed.js
@@ -8,16 +8,14 @@ import Branch from '../models/Branch.js';
 import User from '../models/User.js';
 import Reservation from '../models/Reservation.js';
 
-// Horarios de apertura y cierre para las sucursales
 const openingHours = ['08:00', '08:30', '09:00', '09:30', '10:00'];
 const closingHours = ['18:00', '18:30', '19:00', '19:30', '20:00'];
 
-const saltRounds = 10; // Número de rondas de sal para bcrypt
+const saltRounds = 10;
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
-  // Generar datos para 'Business'
   const businesses = [];
   for (let i = 0; i < 10; i++) {
     const business = await Business.create({
@@ -29,12 +27,11 @@ const seedDatabase = async () => {
     businesses.push(business);
   }
 
-  // Generar datos para 'Branch'
   const branches = [];
   for (let i = 0; i < 20; i++) {
-    const businessId = faker.helpers.arrayElement(businesses).id;
-    const openingTime = faker.helpers.arrayElement(openingHours);
-    const closingTime = faker.helpers.arrayElement(closingHours);
+    const businessId = businesses[faker.datatype.number({ min: 0, max: businesses.length - 1 })].id;
+    const openingTime = openingHours[faker.datatype.number({ min: 0, max: openingHours.length - 1 })];
+    const closingTime = closingHours[faker.datatype.number({ min: 0, max: closingHours.length - 1 })];
 
     const branch = await Branch.create({
       name: faker.company.name(),
@@ -50,7 +47,6 @@ const seedDatabase = async () => {
     branches.push(branch);
   }
 
-  // Generar datos para 'User'
   let userDNIs = [];
   for (let i = 0; i < 50; i++) {
     const dni = faker.datatype.number({ min: 10000000, max: 99999999 });
@@ -69,9 +65,8 @@ const seedDatabase = async () => {
     userDNIs.push(dni);
   }
 
-  // Generar datos para 'Reservation'
   for (let i = 0; i < 100; i++) {
-    const branch = faker.helpers.arrayElement(branches);
+    const branch = branches[faker.datatype.number({ min: 0, max: branches.length - 1 })];
     const time = generateRandomTimeWithinRange(branch.openingTime, branch.closingTime);
 
     await Reservation.create({
@@ -82,14 +77,66 @@ const seedDatabase = async () => {
       clientPhone: faker.datatype.number({ min: 100000000, max: 999999999 }),
       clientEmail: faker.internet.email(),
       branchId: branch.id,
-      userId: faker.helpers.arrayElement(userDNIs)
+      userId: userDNIs[faker.datatype.number({ min: 0, max: userDNIs.length - 1 })]
+    });
+  }
+
+  const specificUsers = [
+    {
+      dni: 11111111,
+      fullName: 'Super User',
+      email: 's@s.com',
+      password: 'super',
+      role: 'super',
+      businessId: null,
+      branchId: null
+    },
+    {
+      dni: 22222222,
+      fullName: 'Admin User',
+      email: 'a@a.com',
+      password: 'admin',
+      role: 'admin',
+      businessId: businesses[0].id,
+      branchId: branches[0].id
+    },
+    {
+      dni: 33333333,
+      fullName: 'Operational User',
+      email: 'o@o.com',
+      password: 'oper',
+      role: 'oper',
+      businessId: businesses[0].id,
+      branchId: branches[0].id
+    },
+    {
+      dni: 44444444,
+      fullName: 'Regular User',
+      email: 'u@u.com',
+      password: 'user',
+      role: 'user',
+      businessId: businesses[0].id,
+      branchId: branches[0].id
+    }
+  ];
+
+  for (const user of specificUsers) {
+    const hashedPassword = await bcrypt.hash(user.password, saltRounds);
+    await User.create({
+      dni: user.dni,
+      fullName: user.fullName,
+      email: user.email,
+      phoneNumber: faker.datatype.number({ min: 100000000, max: 999999999 }),
+      role: user.role,
+      password: hashedPassword,
+      businessId: user.businessId,
+      branchId: user.branchId
     });
   }
 
   console.log('Datos de prueba generados con éxito');
 };
 
-// Funciones auxiliares
 function generateRandomTimeWithinRange(openingTime, closingTime) {
   const startTime = convertTimeToMinutes(openingTime);
   const endTime = convertTimeToMinutes(closingTime);
