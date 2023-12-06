2e5c4983e787a9213163bde559ed3db175c0e69b
javiertrombetta <jitrombetta@hotmail.com>
2023-12-06 14:13:44 -0300

    build: render deploy

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,10 @@
 
 import Sequelize from "sequelize";
 
-const dbName = process.env.DEV_DB_NAME || 'mtw';
-const dbUser = process.env.DEV_DB_USER || null;
-const dbPassword = process.env.DEV_DB_PASSWORD || null;
-const dbHost = process.env.DEV_DB_HOST || 'localhost';
+const dbName = process.env.DEV_DB_NAME;
+const dbUser = process.env.DEV_DB_USER;
+const dbPassword = process.env.DEV_DB_PASSWORD;
+const dbHost = process.env.DEV_DB_HOST;
 
 const sequelize = new Sequelize(dbName, dbUser, dbPassword, {
   host: dbHost,
