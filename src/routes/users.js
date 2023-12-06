b1216c77f681a0f1805699fb0ba0b4f21b96b5ca
dadario23 <dandrada23@gmail.com>
2023-12-06 01:23:16 -0300

    edit get/:dni checkAdminRole

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,12 +1,15 @@
 import express from "express";
-import multer from 'multer';
+import multer from "multer";
 import userController from "../controllers/userController.js";
 import auth from "../middlewares/auth.js";
-import { checkSuperRole, checkAdminRole } from "../middlewares/rolesMiddleware.js";
+import {
+  checkSuperRole,
+  checkAdminRole,
+} from "../middlewares/rolesMiddleware.js";
 import { checkDevEnv } from "../middlewares/envMiddleware.js";
 
 const router = express.Router();
-const upload = multer({ dest: 'uploads/' });
+const upload = multer({ dest: "uploads/" });
 
 //Any client not logged in
 router.post("/register", userController.register);
@@ -17,21 +20,42 @@ router.post("/reset-password", userController.mailResetPassword);
 router.post("/send-message", auth, userController.sendMessage);
 router.post("/logout", auth, userController.logout);
 router.put("/me/change-password", auth, userController.changeUserPassword);
-router.put("/me", auth, upload.single('photo'), userController.updateUser);
+router.put("/me", auth, upload.single("photo"), userController.updateUser);
 router.get("/me", auth, userController.me);
 router.delete("/me", auth, userController.deleteMe);
 //Admin
-router.put("/:dni/depromote", auth, checkAdminRole, userController.depromoteOpertoUserByDni);
+router.put(
+  "/:dni/depromote",
+  auth,
+  checkAdminRole,
+  userController.depromoteOpertoUserByDni
+);
 router.get("/oper", auth, checkAdminRole, userController.getAllOpersByBusiness);
 //Super
-router.post("/:dni/reset-password", auth, checkSuperRole, userController.adminResetPassword);
-router.post("/:dni/assign-role", auth, checkSuperRole, userController.assignRoleToUser);
+router.post(
+  "/:dni/reset-password",
+  auth,
+  checkSuperRole,
+  userController.adminResetPassword
+);
+router.post(
+  "/:dni/assign-role",
+  auth,
+  checkSuperRole,
+  userController.assignRoleToUser
+);
 router.post("/", auth, checkSuperRole, userController.createUser);
-router.put("/:dni", auth, checkSuperRole, upload.single('photo'), userController.updateUserByDni);
-router.get("/:dni", auth, checkSuperRole, userController.getUserByDni);
+router.put(
+  "/:dni",
+  auth,
+  checkSuperRole,
+  upload.single("photo"),
+  userController.updateUserByDni
+);
+router.get("/:dni", auth, checkAdminRole, userController.getUserByDni);
 router.get("/", auth, checkSuperRole, userController.getAllUsers);
 router.delete("/:dni", auth, checkSuperRole, userController.deleteUserByDni);
 //Oper
-router.post('/users/assign', userController.assignBusinessAndBranches);
+router.post("/users/assign", userController.assignBusinessAndBranches);
 
 export default router;
